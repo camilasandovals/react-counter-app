dda85624b6321ab84a8d6b5b3b22ee6e845df10c
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-13 16:06:07 -0400

    react counter app created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer (){
+
+    const githubURL = "https//:github.com/milasandovals/react-counter-app"
+    
+    return (
+        <footer>
+            <p><small>&copy; 2023</small></p>
+            <a href="{githubURL}" target="_blank" rel="noreferrer">See Code in Github</a>
+        </footer>
+    )
+}
